d6838c64dabe7f0f52a3c3369e6de6da9259a72f
CesarV <vallesc217@gmail.com>
2021-05-19 23:53:59 -0500

    2do cambio for.sh

--- for.sh
+++ for.sh
@@ -1,6 +1,6 @@
-#ejemplo para for / enviando a github para prueba de CI
+#ejemplo para for / enviando a 2 cambio github para prueba de CI
  
-for numero in {1..20..1};
+for numero in {1..20..3};
 do
  echo Este es la secuencia del numero: $numero
 done
